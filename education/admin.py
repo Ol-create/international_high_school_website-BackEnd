7f4f574c58aee2b4d1e1d55c0198cfebcba22401
Ol-create <oluola96@gmail.com>
2023-05-02 06:23:37 +0100

    register app to admin site

--- education/admin.py
+++ education/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Student, Course, NewsAndEvent, ComputerBaseTest, AcedemicReport, Alumni
+
 
 # Register your models here.
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(NewsAndEvent)
+admin.site.register(ComputerBaseTest)
+admin.site.register(AcedemicReport)
+admin.site.register(Alumni)
