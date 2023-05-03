24aa52fb13a705b265a7f8481985801f14c61a5a
Ol-create <oluola96@gmail.com>
2023-05-03 18:45:38 +0100

    add discipline, class_level to student model

--- education/admin.py
+++ education/admin.py
@@ -6,6 +6,13 @@ from .models import Student, Course, NewsAndEvent, ComputerBaseTest, AcedemicRep
 admin.site.register(Student)
 admin.site.register(Course)
 admin.site.register(NewsAndEvent)
-admin.site.register(ComputerBaseTest)
+# admin.site.register(ComputerBaseTest)
 admin.site.register(AcedemicReport)
 admin.site.register(Alumni)
+
+# ComputerBaseTest
+class ComputerBaseTestAdmin(admin.ModelAdmin):
+    list_display = ['test_name', 'test_time', 'test_date', 'score'] # Customize the list of fields displayed in the list view
+    list_filter = ['course__course_name','test_date', 'score'] # Add filters to the list view
+
+admin.site.register(ComputerBaseTest, ComputerBaseTestAdmin) # Register the CBT model with the admin site
--- education/models.py
+++ education/models.py
@@ -13,12 +13,45 @@ class Student(models.Model):
         (PREFER_NOT_TO_SAY_TYPE, "Prefer not to say")
     ]
 
+    #Model choice for class level like SS3, SS2, and SS1
+    SENIOR_CLASS_ONE = 'SS1'
+    SENIOR_CLASS_TWO = 'SS2'
+    SENIOR_CLASS_THREE = 'SS3'
+
+    SENIOR_CLASS_LEVEL = [
+        (SENIOR_CLASS_ONE, "Senior Scondary I"),
+        (SENIOR_CLASS_TWO, "Senior Scondary II"),
+        (SENIOR_CLASS_THREE, "Senior Scondary III"),
+    ]
+
+    #Model Discipline Choice
+
+     #Model choice for class level like SS3, SS2, and SS1
+    ART_CLASS = 'ART'
+    COMMERCIAL_CLASS = 'CMR'
+    SCIENCE_CLASS = 'SCI'
+
+    DISCIPLINE_CHOICE = [
+        (ART_CLASS, "Art Subject"),
+        (COMMERCIAL_CLASS, "Commercial Subject"),
+        (SCIENCE_CLASS, "Science Subject"),
+    ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+    profile_image = models.ImageField(null=True)
+    class_level = models.CharField(max_length=3, 
+                                   choices=SENIOR_CLASS_LEVEL, 
+                                   default=SENIOR_CLASS_ONE )
+    discipline = models.CharField(max_length=3, 
+                                  choices=DISCIPLINE_CHOICE, 
+                                  default=COMMERCIAL_CLASS)
     birthday = models.DateField(auto_now_add=True)
     email = models.EmailField(unique=True)
     address = models.CharField(max_length=255)
-    gender = models.CharField(max_length=2, choices=SEX_TYPE_CHOICE, default=PREFER_NOT_TO_SAY_TYPE)
+    gender = models.CharField(max_length=2, 
+                              choices=SEX_TYPE_CHOICE, 
+                              default=PREFER_NOT_TO_SAY_TYPE)
 
 class Course(models.Model):
     course_name = models.CharField(max_length=255)
