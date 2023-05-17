d9efc27885ca562c7020d67aa9b16a9efc92b161
Ol-create <oluola96@gmail.com>
2023-05-17 11:52:24 +0100

    add list_filter and list_per_page to student admin

--- education/admin.py
+++ education/admin.py
@@ -10,6 +10,8 @@ admin.site.site_header = 'International School Administration'
 class studentAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'discipline', 'class_level', 'gender']
     ordering = ['first_name']
+    list_filter = ['discipline', 'class_level', 'gender']
+    list_per_page = 15
 
     def full_name(self, student:Student):
         return f'{student.first_name} {student.last_name}'
@@ -19,10 +21,13 @@ class AlumniAdmin(admin.ModelAdmin):
     list_display = ['get_student_name', 
                     'get_student_email',
                     'phone_number', 
-                    'current_occupation'
+                    'current_occupation',
+                    'get_student_gender',
                     ]
     
     list_per_page = 20
+
+    list_filter = ['current_occupation','graduation_year',]
 
     def get_student_name(self, obj):
         return f'{obj.student.first_name} {obj.student.last_name}'
@@ -34,12 +39,19 @@ class AlumniAdmin(admin.ModelAdmin):
     
     get_student_email.short_description = 'Student Email'
 
+    def get_student_gender(self, obj):
+        return obj.student.gender
+    
+    get_student_gender.short_description = 'Gender'
+    
+   
 admin.site.register(Course)
 admin.site.register(AcedemicReport)
 
 # ComputerBaseTest
 class ComputerBaseTestAdmin(admin.ModelAdmin):
     list_display = ['examination_name', 'test_date', 'score', 'grade'] # Customize the list of fields displayed in the list view
-    list_filter = ['course__course_name','test_date', 'score'] # Add filters to the list view
+    list_filter = ['examination_name','test_date'] # Add filters to the list view
+    search_fields = ('examination_name',)
 
 admin.site.register(ComputerBaseTest, ComputerBaseTestAdmin) # Register the CBT model with the admin site
