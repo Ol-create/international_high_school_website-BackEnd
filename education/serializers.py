2845e8f9eaa9b6c11cc1ce62609477a1754cefc7
Ol-create <oluola96@gmail.com>
2023-06-05 04:24:18 +0100

    Add Alumni_list views

--- education/serializers.py
+++ education/serializers.py
@@ -5,31 +5,36 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'first_name', 'last_name', 'student_identification_number', 
-                  'class_level', 'discipline', 'email', 'address', 'birthday', 'gender']
+                  'class_level', 'discipline', 'email', 'address', 'birthday', 'gender', 'alumni_student']
+        alumni_student = 'AlumniSerializer()'
     
 
 class CourseSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = Course
         fields = ['id', 'course_name', 'description', 'score']
 
 class NewsAndEventSerializer(serializers.ModelSerializer):
-    class meta: 
+    class Meta: 
         model = NewsAndEvent
-        fields = ['id', 'author', 'news_title', 'description', 'content', 'news_date', '']
+        fields = ['id', 'author', 'news_title', 'description', 'content', 'news_date', 'news_time']
 
 class ComputerBaseTestSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = ComputerBaseTest
         fields = ['id', 'examination_type_choice', 'grade_choice', 'examination_name', 'student',
                    'test_date', 'grade', 'score']
         
 class AcedemicReportSerialiser(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = AcedemicReport
         fields = ['id', 'school_term_choice', 'school_term', 'report_date', 'student', 'course']
         
 class AlumniSerializer(serializers.ModelSerializer):
-    class meta:
+    student = serializers.PrimaryKeyRelatedField(
+        queryset=Student.objects.all()
+    )
+
+    class Meta:
         model = Alumni
         fields = ['student', 'phone_number', 'awards', 'graduation_year', 'current_occupation']
--- education/views.py
+++ education/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
-from .models import Student
-from .serializers import StudentSerializer
+from .models import Student, Alumni
+from .serializers import StudentSerializer, AlumniSerializer
 
 # Create your views here.
+#Get all student data
 @api_view()
 def student_list(request):
     students = Student.objects.all()
@@ -18,6 +19,14 @@ def student_detail(request, id):
     student = get_object_or_404(Student, pk=id)
     serializer = StudentSerializer(student)
     return Response(serializer.data)
+
+#Get all alumni data
+@api_view()
+def alumni_list(request):
+    alumni = Alumni.objects.all()
+    serialiser = AlumniSerializer(alumni, many=True)
+    return Response(serialiser.data)
+
 
     
     
