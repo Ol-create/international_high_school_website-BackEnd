6e4937f2024bc5ad0b64ea66a0df68cdb5b949c4
Ol-create <oluola96@gmail.com>
2023-06-05 03:34:24 +0100

    Serialize education model

--- education/serializers.py
+++ education/serializers.py
@@ -8,3 +8,28 @@ class StudentSerializer(serializers.ModelSerializer):
                   'class_level', 'discipline', 'email', 'address', 'birthday', 'gender']
     
 
+class CourseSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Course
+        fields = ['id', 'course_name', 'description', 'score']
+
+class NewsAndEventSerializer(serializers.ModelSerializer):
+    class meta: 
+        model = NewsAndEvent
+        fields = ['id', 'author', 'news_title', 'description', 'content', 'news_date', '']
+
+class ComputerBaseTestSerializer(serializers.ModelSerializer):
+    class meta:
+        model = ComputerBaseTest
+        fields = ['id', 'examination_type_choice', 'grade_choice', 'examination_name', 'student',
+                   'test_date', 'grade', 'score']
+        
+class AcedemicReportSerialiser(serializers.ModelSerializer):
+    class meta:
+        model = AcedemicReport
+        fields = ['id', 'school_term_choice', 'school_term', 'report_date', 'student', 'course']
+        
+class AlumniSerializer(serializers.ModelSerializer):
+    class meta:
+        model = Alumni
+        fields = ['student', 'phone_number', 'awards', 'graduation_year', 'current_occupation']
